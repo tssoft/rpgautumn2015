6d292b5c236eb9292ee4cd64d723d0e57aedc8a3
Aionoff <Aionoff62@mail.ru>
2015-10-23 21:11:34 +0300

    Решение задачи Эйлера 10

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -8,8 +8,38 @@ namespace Euler
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            int count = 2000000;
+            long sum = 0;
+
+            for(int i = 1; i < count; i++)
+            {
+                if (isBelow(i))
+                    sum += i;                
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            
         }
+
+        private static bool isBelow(int number)
+        {
+            int boundary = (int) Math.Floor(Math.Sqrt(number));
+
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;  
+        }
+
     }
 }
