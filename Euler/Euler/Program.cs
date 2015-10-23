42cdce16afc868ea0e44fc70563867daa04961b0
kondr1 <kondr179@rambler.ru>
2015-10-23 21:44:00 +0300

    добавлен не работающий класс с решением 4-й проблемы проблемы (problem_4)

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,44 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    static class problem_4
+    {
+        static public void solution()
+        {
+            int number1, number2, result;
+            number1 = 999;
+            number2 = 999;
+            while (number1 > 0)
+            {
+                while (number2 > 0)
+                {
+                    result = number1 * number2;
+                    if (problem_4.polindrom(result))
+                    {
+
+                    }
+                    number2--;
+                }
+                number1--;
+            }
+        }
+        static public bool polindrom(int number)
+        {
+            string pol = number.ToString();
+            if (problem_4.even(pol.Length))
+            {
+                
+            }
+        }
+
+        static public bool even(int number)
+        {
+            if ((number % 2) == 0) return (true);
+            else return (false);
         }
     }
 }
