fcd94931ae22a2d7c0ec6a7a64e1e096f9b75b68
levdok <evdokimova.l@hotmail.com>
2015-10-29 20:57:06 +0300

    Форматирование класса EulerTask.cs

--- Euler/Euler/EulerTask.cs
+++ Euler/Euler/EulerTask.cs
@@ -14,14 +14,14 @@ namespace Euler
         /// <summary>
         /// Суммирует простые числа от единицы до указанного числа
         /// </summary>
-        /// <param name="number">Число, до которого производится 
+        /// <param name="parNumber">Число, до которого производится 
         /// суммирование</param>
         /// <returns>сумму простых чисел от единицы до number</returns>
-        public static long SumarizePrimesBelow(long number)
+        public static long SumarizePrimesBelow(long parNumber)
         {
             long sum = 0;
 
-            for (int i = 1; i < number; i++)
+            for (int i = 1; i < parNumber; i++)
             {
                 if (IsBelow(i))
                     sum += i;
@@ -33,19 +33,19 @@ namespace Euler
         /// <summary>
         /// Проверяет является ли число простым
         /// </summary>
-        /// <param name="number">Проверяемое число</param>
+        /// <param name="parNumber">Проверяемое число</param>
         /// <returns>true, если число просто, false, если число не является 
         /// простым</returns>
-        private static bool IsBelow(int number)
+        private static bool IsBelow(int parNumber)
         {
-            int boundary = (int)Math.Floor(Math.Sqrt(number));
+            int boundary = (int)Math.Floor(Math.Sqrt(parNumber));
 
-            if (number == 1) return false;
-            if (number == 2) return true;
+            if (parNumber == 1) return false;
+            if (parNumber == 2) return true;
 
             for (int i = 2; i <= boundary; ++i)
             {
-                if (number % i == 0) return false;
+                if (parNumber % i == 0) return false;
             }
 
             return true;
